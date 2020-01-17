06c223cd453db6b2889fccf4289b16f8a62fc5c0
xieqingtian <1255699673@qq.com>
2020-01-17 12:26:22 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import Config from 'webpack-chain';
+
 class A {
     say() {
         console.log('hello world');
@@ -21,3 +23,17 @@ const foo: Foo = {
 console.log(foo?.a?.b?.c);
 
 console.log(process.argv);
+
+const config = new Config();
+
+console.log(
+    JSON.stringify(
+        config
+            .entry('main')
+            .add('./src')
+            .end()
+            .toConfig(),
+        null,
+        2
+    )
+);
