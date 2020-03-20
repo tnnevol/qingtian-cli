aafba351a50137ff5c91cc1df2e13255ae743e21
xieqingtian <1255699673@qq.com>
2020-03-20 12:24:58 +0800

    sdsd

--- qt.config.js
+++ qt.config.js
@@ -1,43 +1,3 @@
-# qt-cli
-
-> 一个专注于 react + typescript + electron 开发的工作流
-
-# Install
-
-```
-yarn add global qingtian-cli
-```
-
-or
-
-```
-npm install qingtian-cli -g
-```
-
-# Contains
-
--   web 以及 electron 项目初始化 ✅
--   常用代码生成 ✅
--   一键进入 webpack 开发模式和打包模式 ✅
--   内置 typescript 支持 ✅
--   内置 sass 支持 ✅
--   内置 postcss 支持 ✅
--   webpack 打包分析 ✅
--   css modules ✅
--   支持多页应用构建 ✅
-
-# Commands
-
--   **qt new**： 创建 web 或 electron 项目
--   **qt dev**： 启动项目开发
--   **qt build**： 生产环境构建
--   **qt generate**： 常用代码生成
-
-# Config
-
-新建一个 `qt.config.js` 置于项目根目录，内容如下
-
-```js
 module.exports = {
     electron: {
         rendererEntry: './src/renderer/index.tsx', // 配置渲染进程入口
@@ -68,4 +28,3 @@ module.exports = {
     // webpack配置扩展
     chainWebpack: config => {}
 };
-```
